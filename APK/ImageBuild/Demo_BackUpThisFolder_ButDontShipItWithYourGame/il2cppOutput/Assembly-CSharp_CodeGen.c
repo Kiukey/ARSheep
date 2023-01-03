de00d9c015559b4cffeb4bf93aacfc1a1915ddb8
Kiukey <theodu8150@gmail.com>
2023-01-03 14:59:17 +0100

    pushing images

---
+++ APK/ImageBuild/Demo_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,174 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mF5AE519F086E52A01EA091115062D84D9F094F34 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_mF4BA6DDB611A3FD966C8B9494AE6B3EB8647CEAD (void);
+// 0x00000011 UnityEngine.XR.ARFoundation.ARTrackedImageManager ImageTracking::get_ImageManager()
+extern void ImageTracking_get_ImageManager_mB9167D082802B94F3D40A1592065C9D25AA2902F (void);
+// 0x00000012 System.Void ImageTracking::set_ImageManager(UnityEngine.XR.ARFoundation.ARTrackedImageManager)
+extern void ImageTracking_set_ImageManager_m4BF581E9E65C90F8F99524622D4318279EEFECCD (void);
+// 0x00000013 UnityEngine.XR.ARSubsystems.XRReferenceImageLibrary ImageTracking::get_ImageLibrary()
+extern void ImageTracking_get_ImageLibrary_mA5DC16FA8A2DFD989956A29CE9937581CA2EC246 (void);
+// 0x00000014 System.Void ImageTracking::set_ImageLibrary(UnityEngine.XR.ARSubsystems.XRReferenceImageLibrary)
+extern void ImageTracking_set_ImageLibrary_m80A3F2FF2C1BF1BEE390D9832A9CA04A7D0F5F9C (void);
+// 0x00000015 UnityEngine.GameObject ImageTracking::get_onePrefab()
+extern void ImageTracking_get_onePrefab_m2CC21F714C536DF554F0DCC780B416DC7710AE41 (void);
+// 0x00000016 System.Void ImageTracking::set_onePrefab(UnityEngine.GameObject)
+extern void ImageTracking_set_onePrefab_mDF94AA447B744B654A09E84D6BF51352CB478336 (void);
+// 0x00000017 UnityEngine.GameObject ImageTracking::get_spawnedOnePrefab()
+extern void ImageTracking_get_spawnedOnePrefab_m4D2F6068C3A715C9FF6B299252D8B260F6007B44 (void);
+// 0x00000018 System.Void ImageTracking::set_spawnedOnePrefab(UnityEngine.GameObject)
+extern void ImageTracking_set_spawnedOnePrefab_m4681870E45A9EEDC602A47ACC7C83632FCD08C3C (void);
+// 0x00000019 UnityEngine.GameObject ImageTracking::get_twoPrefab()
+extern void ImageTracking_get_twoPrefab_m97076DA1667C712D3AD40872677AA69F00AAE6BA (void);
+// 0x0000001A System.Void ImageTracking::set_twoPrefab(UnityEngine.GameObject)
+extern void ImageTracking_set_twoPrefab_m6C4D65B8A1680431F9AE6A793FEB4DF1FDA8978E (void);
+// 0x0000001B UnityEngine.GameObject ImageTracking::get_spawnedTwoPrefab()
+extern void ImageTracking_get_spawnedTwoPrefab_m737E1C9DBD7C221EA2623919A05A7E031AF85D34 (void);
+// 0x0000001C System.Void ImageTracking::set_spawnedTwoPrefab(UnityEngine.GameObject)
+extern void ImageTracking_set_spawnedTwoPrefab_mD2D75BD47B2AD223794BF828EBE23BE9106715F0 (void);
+// 0x0000001D System.Void ImageTracking::OnEnable()
+extern void ImageTracking_OnEnable_m6DEDEE1663EC7AA2AB1000CF58776EC231A744B7 (void);
+// 0x0000001E System.Void ImageTracking::OnDisable()
+extern void ImageTracking_OnDisable_m62F1D5572FB9F965AE9E9D881D14D2747FFEE797 (void);
+// 0x0000001F System.Void ImageTracking::ImageManagerOnTrackedImagesChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void ImageTracking_ImageManagerOnTrackedImagesChanged_m5C4AABB694AC53B59CBE491549C965582EDCFD19 (void);
+// 0x00000020 System.Int32 ImageTracking::NumberOfTrackedImages()
+extern void ImageTracking_NumberOfTrackedImages_mDD0F6C9D25B85575AEB0E715506055DB32F0E615 (void);
+// 0x00000021 System.Void ImageTracking::.ctor()
+extern void ImageTracking__ctor_m88C12006AC3BAED3DAA4F0E0544DAA5294D96A5A (void);
+static Il2CppMethodPointer s_methodPointers[33] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mF5AE519F086E52A01EA091115062D84D9F094F34,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_mF4BA6DDB611A3FD966C8B9494AE6B3EB8647CEAD,
+	ImageTracking_get_ImageManager_mB9167D082802B94F3D40A1592065C9D25AA2902F,
+	ImageTracking_set_ImageManager_m4BF581E9E65C90F8F99524622D4318279EEFECCD,
+	ImageTracking_get_ImageLibrary_mA5DC16FA8A2DFD989956A29CE9937581CA2EC246,
+	ImageTracking_set_ImageLibrary_m80A3F2FF2C1BF1BEE390D9832A9CA04A7D0F5F9C,
+	ImageTracking_get_onePrefab_m2CC21F714C536DF554F0DCC780B416DC7710AE41,
+	ImageTracking_set_onePrefab_mDF94AA447B744B654A09E84D6BF51352CB478336,
+	ImageTracking_get_spawnedOnePrefab_m4D2F6068C3A715C9FF6B299252D8B260F6007B44,
+	ImageTracking_set_spawnedOnePrefab_m4681870E45A9EEDC602A47ACC7C83632FCD08C3C,
+	ImageTracking_get_twoPrefab_m97076DA1667C712D3AD40872677AA69F00AAE6BA,
+	ImageTracking_set_twoPrefab_m6C4D65B8A1680431F9AE6A793FEB4DF1FDA8978E,
+	ImageTracking_get_spawnedTwoPrefab_m737E1C9DBD7C221EA2623919A05A7E031AF85D34,
+	ImageTracking_set_spawnedTwoPrefab_mD2D75BD47B2AD223794BF828EBE23BE9106715F0,
+	ImageTracking_OnEnable_m6DEDEE1663EC7AA2AB1000CF58776EC231A744B7,
+	ImageTracking_OnDisable_m62F1D5572FB9F965AE9E9D881D14D2747FFEE797,
+	ImageTracking_ImageManagerOnTrackedImagesChanged_m5C4AABB694AC53B59CBE491549C965582EDCFD19,
+	ImageTracking_NumberOfTrackedImages_mDD0F6C9D25B85575AEB0E715506055DB32F0E615,
+	ImageTracking__ctor_m88C12006AC3BAED3DAA4F0E0544DAA5294D96A5A,
+};
+static const int32_t s_InvokerIndices[33] = 
+{
+	5148,
+	4161,
+	5243,
+	5243,
+	5243,
+	5243,
+	8328,
+	5189,
+	4194,
+	5243,
+	5243,
+	5243,
+	4055,
+	4161,
+	5243,
+	8328,
+	5148,
+	4161,
+	5148,
+	4161,
+	5148,
+	4161,
+	5148,
+	4161,
+	5148,
+	4161,
+	5148,
+	4161,
+	5243,
+	5243,
+	4065,
+	5121,
+	5243,
+};
+extern const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationAssemblyU2DCSharp;
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	33,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	&g_DebuggerMetadataRegistrationAssemblyU2DCSharp,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
